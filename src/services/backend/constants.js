092d3949378ba27caf627affda760406a1a9eb57
Omar Eldeeb <40169383+omareldeeb@users.noreply.github.com>
2020-04-20 15:22:34 +0200

    Move backend base url to .env (#20)

--- src/services/backend/constants.js
+++ src/services/backend/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://api.encore-fm.com';
+export const API_BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL;
 
 // codes of errors received from backend
 export const SESSION_NOT_FOUND_ERROR = "SessionNotFoundError";
