725d81b832550b6e41099f29f317fa3450c36c37
omareldeeb <omareldeeb00@live.com>
2020-04-21 22:20:12 +0200

    Fix delete session and leave session services

--- src/reducers/isLogged.js
+++ src/reducers/isLogged.js
@@ -1,4 +1,4 @@
-import {AUTH_FAILURE, CREATE_SUCCESS, JOIN_SUCCESS} from "../actions/user";
+import {AUTH_FAILURE, CREATE_SUCCESS, DELETE_SESSION_SUCCESS, JOIN_SUCCESS, LEAVE_SUCCESS} from "../actions/user";
 import {REQUEST_NOT_AUTHORIZED_ERROR} from "../services/backend/constants";
 
 export default (state = false, action) => {
@@ -9,6 +9,9 @@ export default (state = false, action) => {
     case AUTH_FAILURE:
       // set isLogged to false if backend can't find username/session/secret
       return action.error.error !== REQUEST_NOT_AUTHORIZED_ERROR;
+    case LEAVE_SUCCESS:
+    case DELETE_SESSION_SUCCESS:
+      return false;
     default:
       return state;
   }
