bd25b281cb3a911136cdc86bad9cb1df72c894e2
omareldeeb <omareldeeb00@live.com>
2020-04-21 21:51:02 +0200

    Add null check to search result album

--- src/services/spotify/FetchSongs.js
+++ src/services/spotify/FetchSongs.js
@@ -27,7 +27,7 @@ class FetchSongs {
           trackID: track.id,
           albumName: track.album.name,
           artists: track.artists.map(artist => artist.name),
-          coverUrl: track.album.images[0].url,
+          coverUrl: track.album.images[0] ? track.album.images[0].url : null,
           trackDuration: track.duration_ms
         }
       )));
