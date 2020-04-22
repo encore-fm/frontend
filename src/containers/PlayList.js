2bfb0d9fb3ddd34df1b91026310a26cd229d490b
Omar Eldeeb <40169383+omareldeeb@users.noreply.github.com>
2020-04-22 22:32:42 +0200

    Move SSE handling to MainView.js (#18)

--- src/containers/PlayList.js
+++ src/containers/PlayList.js
@@ -1,10 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
-import {fetchPlaylist, setPlaylist} from '../actions/playlist';
+import {fetchPlaylist} from '../actions/playlist';
 import PlayListElement from '../components/PlayListElement';
 import {voteSong} from "../actions/voteSong";
-import {API_BASE_URL} from "../services/backend/constants";
-import parsePlaylist from "../services/backend/helpers/parsePlaylist";
 
 import './PlayList.scss';
 
@@ -13,28 +11,9 @@ const PlayList = (props) => {
 
   useEffect(() => {
       props.dispatch(fetchPlaylist(user));
-
-      // register sse event source
-      const eventSource = new EventSource(`${API_BASE_URL}/events/${user.username}/${user.sessionID}`);
-
-      // listen for incoming playlist change events and set the state playlist accordingly
-      eventSource.addEventListener(
-        'sse:playlist_change',
-        e => handlePlaylistChange(JSON.parse(e.data))
-      );
-
-      // clean up when component unmounts
-      return () => eventSource.close();
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
-
-  const handlePlaylistChange = data => {
-    const newPlaylist = parsePlaylist(data);
-
-    props.dispatch(setPlaylist(newPlaylist));
-  };
 
   const handleVote = (songID, isUpvote) => {
     const voteAction = isUpvote ? 'up' : 'down';
