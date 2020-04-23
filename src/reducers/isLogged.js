451f3c41f95ce016c299aa8dcdeb2ede90e8caf3
Omar Eldeeb <40169383+omareldeeb@users.noreply.github.com>
2020-04-24 00:10:52 +0200

    Catch auth error in isLogged and set isLogged state to false (#24)

--- src/reducers/isLogged.js
+++ src/reducers/isLogged.js
@@ -2,13 +2,14 @@ import {AUTH_FAILURE, CREATE_SUCCESS, DELETE_SESSION_SUCCESS, JOIN_SUCCESS, LEAV
 import {REQUEST_NOT_AUTHORIZED_ERROR} from "../services/backend/constants";
 
 export default (state = false, action) => {
+  // set isLogged to false if backend ever returns a request not authorized error
+  if (action.error && action.error.error === REQUEST_NOT_AUTHORIZED_ERROR)
+    return false;
+
   switch (action.type) {
     case CREATE_SUCCESS:
     case JOIN_SUCCESS:
       return true;
-    case AUTH_FAILURE:
-      // set isLogged to false if backend can't find username/session/secret
-      return action.error.error !== REQUEST_NOT_AUTHORIZED_ERROR;
     case LEAVE_SUCCESS:
     case DELETE_SESSION_SUCCESS:
       return false;
