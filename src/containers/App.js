09aea7e828c6cb4e0c06f95388f06f16ce4c7054
Anton Baumann <antonbaumann4@gmail.com>
2020-04-24 23:56:06 +0200

    fix desynchronize on unmount

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Header from './Header';
@@ -16,36 +16,41 @@ import {desynchronize} from "../actions/user";
 import {connect} from "react-redux";
 
 
-class App extends Component {
+const App = (props) => {
 
-  constructor(props) {
-    super(props);
-  }
+  const {user} = props;
 
-  componentWillUnmount() {
-    if (this.props.user) this.props.dispatch(desynchronize(this.props.user)); // desynchronize user when unmounting
-  }
+  useEffect(() => {
+    const desynchronizeFn = () => {
+      if (user) props.dispatch(desynchronize(user));
+    };
 
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header/>
-          <Switch>
-            <Route exact path="/" component={WelcomeView}/>
-            <Route path="/join/:sessionID?" component={JoinSessionView}/>
-            <Route path="/join" component={JoinSessionView}/>
-            <Route path="/create" component={CreateSessionView}/>
-            <Route path="/player" component={MainView}/>
-            <Route path="/add" component={MainView}/>
-            <Route path="/profile" component={UserList}/>
-            <Route path="/get-started" component={GetStartedView}/>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+    window.addEventListener('beforeunload', desynchronizeFn);
+    return () => {
+      window.removeEventListener('beforeunload', desynchronizeFn);
+    }
+  }, [user]);
+
+
+  return (
+    <Router>
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={WelcomeView}/>
+          <Route path="/join/:sessionID?" component={JoinSessionView}/>
+          <Route path="/join" component={JoinSessionView}/>
+          <Route path="/create" component={CreateSessionView}/>
+          <Route path="/player" component={MainView}/>
+          <Route path="/add" component={MainView}/>
+          <Route path="/profile" component={UserList}/>
+          <Route path="/get-started" component={GetStartedView}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+
+};
 
 export default connect(
   state => ({
