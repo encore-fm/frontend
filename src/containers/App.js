f693384acf9313d1a776b1183dcdad06bdc0e4b8
Anton Baumann <antonbaumann4@gmail.com>
2020-04-25 16:57:55 +0200

    add analytics

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Route, Switch, useHistory} from 'react-router-dom';
 
 import Header from './Header';
 
@@ -17,10 +17,15 @@ import {desynchronize} from "../actions/user";
 import {connect} from "react-redux";
 import {REQUEST_NOT_AUTHORIZED_ERROR} from "../services/backend/constants";
 import SessionNotFoundView from "../views/SessionNotFoundView";
+import ReactGA from "react-ga";
+import {createBrowserHistory} from "history";
 
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID, { debug: process.env.REACT_APP_GA_DEBUG});
+
+const history = createBrowserHistory();
 
 const App = (props) => {
-
   const {user, isLogged, error} = props;
 
   useEffect(() => {
@@ -34,8 +39,21 @@ const App = (props) => {
     }
   }, [user]);
 
+  useEffect(() => {
+    logPageView(history);
+  }, [history]);
+
+  const logPageView = (history) => {
+    history.listen((location) => {
+      const page = location.pathname || window.location.pathname;
+      ReactGA.set({ page: page });
+      ReactGA.pageview(page);
+      console.log(`Page View logged for: ${page}`);
+    });
+  };
+
   return (
-    <Router>
+    <Router history={history}>
       <div className="App">
         <Header/>
         <Switch>
