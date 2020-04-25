b0252cf89af7b7638b74f2417a121a0bd214c333
Anton Baumann <antonbaumann4@gmail.com>
2020-04-25 16:57:55 +0200

    add analytics

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 
 import Header from './Header';
@@ -16,14 +16,45 @@ import GetStartedView from "../views/GetStartedView";
 import {connect} from "react-redux";
 import {REQUEST_NOT_AUTHORIZED_ERROR} from "../services/backend/constants";
 import SessionNotFoundView from "../views/SessionNotFoundView";
+import ReactGA from "react-ga";
+import {createBrowserHistory} from "history";
+import {desynchronize} from "../actions/user";
+
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID, {debug: process.env.REACT_APP_GA_DEBUG});
+
+const history = createBrowserHistory();
 
 
 const App = (props) => {
+  const {user, isLogged, error} = props;
 
-  const {error} = props;
+  useEffect(() => {
+    const desynchronizeFn = () => {
+      if (isLogged && user && user.spotifyAuthorized) props.dispatch(desynchronize(user));
+    };
+
+    window.addEventListener('beforeunload', desynchronizeFn);
+    return () => {
+      window.removeEventListener('beforeunload', desynchronizeFn);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    logPageView(history);
+  }, [history]);
+
+  const logPageView = (history) => {
+    history.listen((location) => {
+      const page = location.pathname || window.location.pathname;
+      ReactGA.set({page: page});
+      ReactGA.pageview(page);
+      console.log(`Page View logged for: ${page}`);
+    });
+  };
 
   return (
-    <Router>
+    <Router history={history}>
       <div className="App">
         <Header/>
         <Switch>
@@ -38,7 +69,7 @@ const App = (props) => {
           <Route path="/get-started" component={GetStartedView}/>
           <Route path="/session-not-found" component={SessionNotFoundView}/>
         </Switch>
-        {error.error === REQUEST_NOT_AUTHORIZED_ERROR  && <Redirect to="/session-not-found"/>}
+        {error.error === REQUEST_NOT_AUTHORIZED_ERROR && <Redirect to="/session-not-found"/>}
       </div>
     </Router>
   );
