8276594c04949904218918a190e2d92cbd0305eb
omareldeeb <omareldeeb00@live.com>
2020-04-28 20:18:50 +0200

    Fix SessionNotFoundView

--- src/views/SessionNotFoundView.js
+++ src/views/SessionNotFoundView.js
@@ -3,13 +3,18 @@ import {Link, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import ContentWrapper from "../components/ContentWrapper";
 import {REQUEST_NOT_AUTHORIZED_ERROR, SESSION_NOT_FOUND_ERROR} from "../services/backend/constants";
+import {logOut} from "../actions/user";
 
 const SessionNotFoundView = (props) => {
-  const {error} = props;
+  const {user, error} = props;
   // restrict access to this view
   // user should only land here if their credentials are invalid or if join session id is wrong
-  if (error.error !== REQUEST_NOT_AUTHORIZED_ERROR && error.error !== SESSION_NOT_FOUND_ERROR)
+  if (user && error.error !== REQUEST_NOT_AUTHORIZED_ERROR && error.error !== SESSION_NOT_FOUND_ERROR)
     return <Redirect to="/"/>;
+
+  // clear user details to avoid redirecting back to this page (when authentication fails again)
+  localStorage.clear();
+  props.dispatch(logOut()); // clear user, isLogged and error
 
   return (
     <ContentWrapper>
