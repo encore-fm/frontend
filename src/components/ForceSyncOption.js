5bece0a05c51383540a00740bd783819fd831871
Anton Baumann <antonbaumann4@gmail.com>
2020-05-01 19:05:03 +0200

    -sort user list -sync select

--- src/components/ForceSyncOption.js
+++ src/components/ForceSyncOption.js
@@ -2,16 +2,36 @@ import React, {useState} from 'react';
 
 import './ForceSyncOption.scss';
 
+const SYNC_TYPES = Object.freeze({
+  OFF: 'OFF',
+  AUTO: 'AUTO',
+  ALWAYS: "ALWAYS",
+});
+
+const INFO_TEXT = {
+  OFF: "only vote - don't listen",
+  AUTO: "pause when site is closed - play when site is opened",
+  ALWAYS: "always stay synchronized",
+};
+
 const ForceSyncOption = () => {
-  const [state, setState] = useState(false);
+  const [state, setState] = useState(SYNC_TYPES.AUTO);
 
   const handleChange = (e) => {
-    setState(e.target.checked);
+    setState(e.target.value);
   };
 
   return (
     <div className="ForceSyncOption">
-      always stay synchronized <input type="checkbox" checked={state} onChange={handleChange}/>
+      synchronize option
+      <select value={state} onChange={handleChange}>
+        <option value={SYNC_TYPES.OFF}>off</option>
+        <option value={SYNC_TYPES.AUTO}>auto</option>
+        <option value={SYNC_TYPES.ALWAYS}>always</option>
+      </select>
+      <div className="ForceSyncOption__info">
+        {INFO_TEXT[state]}
+      </div>
     </div>
   )
 };
