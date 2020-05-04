5bcc47ed8f7d19d510385949adb10df70bc9bfbc
Anton Baumann <antonbaumann4@gmail.com>
2020-05-04 22:28:56 +0200

    ask user if he wants to leave old session

--- src/actions/menu.js
+++ src/actions/menu.js
@@ -1,25 +1,13 @@
-import ReactGA from "react-ga";
-
 export const SET_MENU_OPEN = 'SET_MENU_OPEN';
 export const SET_MENU_CLOSED = 'SET_MENU_CLOSED';
 
 export const openMenu = () => {
-  ReactGA.event({
-    category: 'Menu',
-    action: 'open'
-  });
-
   return {
     type: SET_MENU_OPEN,
   }
 };
 
 export const closeMenu = () => {
-  ReactGA.event({
-    category: 'Menu',
-    action: 'close'
-  });
-
   return {
     type: SET_MENU_CLOSED
   }
--- src/containers/JoinSessionForm.js
+++ src/containers/JoinSessionForm.js
@@ -2,33 +2,42 @@ import React, {useEffect, useState} from "react";
 import {connect} from "react-redux";
 import DefaultTextField from "../components/DefaultTextField";
 import Button from "../components/Button";
-import {fetchUserInfo, joinSession} from "../actions/user";
+import {deleteSession, fetchUserInfo, joinSession, leaveSession} from "../actions/user";
 import {useParams, useHistory, Redirect} from "react-router-dom";
 import {fetchSessionInfo} from "../actions/sessionInfo";
 import {SESSION_NOT_FOUND_ERROR} from "../services/backend/constants";
+import {fetchUserList} from "../actions/userList";
 
 
 const STAGES = Object.freeze({
+  INIT: 'INIT',
   NAME: 'NAME',
   AUTH: 'AUTH',
 });
 
+const getAdminName = (users) => {
+  return users.filter(user => user.isAdmin === true)[0];
+};
+
 const JoinSessionForm = (props) => {
   const history = useHistory();
   const {sessionID} = useParams();
-  const {user, isLogged, error, sessionInfo} = props;
+  const {user, isLogged, isFetching, error, sessionInfo, userList} = props;
 
   const [username, setUsername] = useState('');
-  const [stage, setStage] = useState(STAGES.NAME);
+  const [stage, setStage] = useState(STAGES.INIT);
 
   useEffect(() => {
     props.dispatch(fetchSessionInfo(sessionID));
-    if (user) props.dispatch(fetchUserInfo(user));
+    if (user) {
+      props.dispatch(fetchUserInfo(user));
+      props.dispatch(fetchUserList(user));
+    }
   }, []);
 
   useEffect(() => {
     if (!error.error) {
-      if (isLogged || (stage === STAGES.NAME && sessionInfo && username)) {
+      if ((stage === STAGES.NAME && sessionInfo && username)) {
         setStage(STAGES.AUTH);
       }
     }
@@ -37,6 +46,7 @@ const JoinSessionForm = (props) => {
   const redirectAuthorize = () => {
     if (isLogged) {
       window.open(user.authUrl, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+      // history.push('/player');
     }
   };
 
@@ -53,11 +63,23 @@ const JoinSessionForm = (props) => {
     setUsername(event.target.value);
   };
 
+  const handleJoinNewSession = () => {
+    if (user.isAdmin) {
+      if (window.confirm('You are admin of the current session.\nIf you join the new session your current session will be deleted')) {
+        props.dispatch(deleteSession(user));
+      }
+    } else {
+      props.dispatch(leaveSession(user));
+    }
+    setStage(STAGES.NAME);
+    setUsername('');
+  };
+
   const renderStageName = () => {
     // only redirect if we know the session isn't valid
     // !sessionInfo doesn't suffice since the request to fetch the session info is asynchronous.
     if (error.error === SESSION_NOT_FOUND_ERROR)
-      return <Redirect to="/session-not-found" />;
+      return <Redirect to="/session-not-found"/>;
 
     else if (sessionInfo)
       return (
@@ -97,13 +119,36 @@ const JoinSessionForm = (props) => {
     )
   };
 
+  const renderStageInit = () => {
+    if (user && sessionID === user.sessionID) return <Redirect to='/player' />;
+
+    if (user && sessionInfo && userList && userList.length > 0) {
+      return (
+        <div>
+          you are currently in <span className="highlight">{getAdminName(userList).username}</span>'s session.<br/>
+          do you want to join <span className="highlight">{sessionInfo.admin_name}</span>'s session?<br/>
+          <br/>
+          <Button style={{marginRight: '1em'}} type="text" text="stay" onClick={redirectPlayer}/>
+          <Button type="submit" text="join new" onClick={handleJoinNewSession}/>
+        </div>
+      )
+    }
+
+    if (!user) setStage(STAGES.NAME);
+    return ""
+  };
+
+  if (!user && isFetching) return "";
+
   switch (stage) {
+    case STAGES.INIT:
+      return renderStageInit();
     case STAGES.NAME:
       return renderStageName();
     case STAGES.AUTH:
       return renderStageAuth();
     default:
-      return renderStageName();
+      return renderStageInit();
   }
 };
 
@@ -112,6 +157,8 @@ export default connect(
     user: state.user,
     isLogged: state.isLogged,
     sessionInfo: state.sessionInfo,
+    isFetching: state.isFetching,
+    userList: state.userList,
     error: state.error,
   })
 )(JoinSessionForm);
